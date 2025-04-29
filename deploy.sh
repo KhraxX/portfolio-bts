d4e3358e077602d7d0ddcd74cca0a8edb976f554
KhraxX <robwood2806@gmail.com>
2025-04-29 09:21:28 +0100

    fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+sudo systemctl stop nginx
+
 echo "📥 Pull du dépôt Git..."
 git pull
 
 echo "🚚 Déploiement dans /var/www/roudard.fr..."
 sudo rm -rf /var/www/roudard.fr/*
 sudo mv  ./ /var/www/roudard.fr/
+
+sudo systecmctl start nginx
 
 echo "🔄 Reload de Nginx (au cas où)..."
 sudo systemctl reload nginx
